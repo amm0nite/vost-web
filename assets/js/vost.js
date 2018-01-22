7e7adcba873f5b417d1bdf143eef019dd4e17f50
Ammonite <ammonite.myfox@gmail.com>
2018-01-22 22:58:56 +0100

    Use jquery

--- assets/js/vost.js
+++ assets/js/vost.js
@@ -1,28 +1,7 @@
 
-function getJSON(url, next) {
-    let httpRequest = new XMLHttpRequest();
-    httpRequest.onreadystatechange = function () {
-        if (httpRequest.readyState == XMLHttpRequest.DONE) {
-            httpRequest.onreadystatechange = null;
-            if (httpRequest.status == 200) {
-                let data = JSON.parse(httpRequest.responseText);
-                return next(null, data);
-            } else {
-                return next({
-                    code: httpRequest.status,
-                    message: httpRequest.responseText
-                });
-            }
-        }
-    };
-    httpRequest.open('GET', url, true);
-    httpRequest.send(null);
-}
-
 function fetch() {
     let url = 'https://api.github.com/gists/4ff8222a078d9db0096195a46ef2a783';
-    getJSON(url, (err, gist) => {
-        if (err) return console.log(err);
+    $.getJSON(url, (gist) => {
         let shows = JSON.parse(gist.files['vost.json'].content);
         shows.sort((a, b) => {
             return (new Date(a.date)) - (new Date(b.date));
